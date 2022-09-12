8dec4b746a6c6ce35ff238b46411ef175f68a4eb
diegorepassa <diego.ferreira@repassa.com.br>
2022-09-12 13:44:20 -0300

    path fixes

--- src/atoms/Title/styles.ts
+++ src/atoms/Title/styles.ts
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 interface Props {
-    fontSize?: number;
     color?: string;
 }
 
 export const Title = styled.h1<Props>`
-    font-size: ${props => `${props.fontSize}px`};
+    font-size: 40px;
     color: ${props => props.color};
     font-family: 'Circular Std Bold';
     max-width: 620px;
--- src/molecules/Card/index.tsx
+++ src/molecules/Card/index.tsx
@@ -15,25 +15,25 @@ const data: Props[] = [
   {
     id: 1,
     title: "Revistas",
-    icon: "/assets/Revistas.svg",
+    icon: "./assets/Revistas.svg",
     desc: "Fonte de Doutrina de juristas renomados, jurisprudência selecionada e comentários sobre mudanças na Legislação.",
   },
   {
     id: 2,
     title: "Livros",
-    icon: "/assets/Livros.svg",
+    icon: "./assets/Livros.svg",
     desc: "Importantes obras jurídicas contendo estudos em diversas áreas do Direito.",
   },
   {
     id: 3,
     title: "Vídeos",
-    icon: "/assets/Videos.svg",
+    icon: "./assets/Videos.svg",
     desc: "Rica fonte de citação para fundamentação de processos, são gravados durante conferências e palestras exclusivas.",
   },
   {
     id: 4,
     title: "Informativos",
-    icon: "/assets/Infos.svg",
+    icon: "./assets/Infos.svg",
     desc: "Contêm normas, decretos, portarias e a seleção das principais notícias de interesse aos profissionais do Direito.",
   },
 ];
